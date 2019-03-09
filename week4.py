a500b2d919ca9f5c18060f844148cad5d5c3d804
Ben <ben.x.rees@gmail.com>
2019-03-09 15:19:14 +0000

    Homework 2

---
+++ week4.py
@@ -0,0 +1,39 @@
+import math
+#test whether a number is prime - PART 1
+def isprime(n):
+    if n >= 2:
+        for i in range(2,int(math.floor(math.sqrt(n)+1))):
+            if ( n % i == 0):
+                return False
+        return True
+
+#find the first value for which mersenne Primes fail - Part 2
+def wrong_prime() :
+    p=1
+    while(True) :
+        if(isprime(p)) :
+            if not(isprime((2**p)-1)) :
+                return p
+        p=p+1
+#find the mersenne primes under n_max
+def mersenne_prime(n_max) :
+    list_of_primes = list()
+    for p in range(1,n_max) :
+        if(isprime(p)) :
+            if(isprime((2**p)-1)) :
+                list_of_primes.append((2**p)-1)
+    return list_of_primes
+
+
+
+
+
+
+
+
+
+
+
+
+
+
